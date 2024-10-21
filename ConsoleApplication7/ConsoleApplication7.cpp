6b732d371359d21f22b2415bec189ce5683d597f
User <User@WIN-GDRLM5EKCOG>
2024-10-21 09:39:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int reverse_number(int n, int reversed = 0) {
+    if (n == 0) {
+        return reversed;
+    }
+    return reverse_number(n / 10, reversed * 10 + n % 10);
+}
+int main() {
+    int number = 321;
+    int reversed_number = reverse_number(number);
+    cout << reversed_number << endl;
+    return 0;
+}
