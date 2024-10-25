ab1a627a73b6dd8ab4e701c33edd65b6d8bec743
s-ig097 <igum503@gmail.com>
2024-10-25 12:19:46 +0500

    добавил функцию аккерман

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -4,10 +4,23 @@ int fibonachi(int num) {
     if (num == 0 || num == 1) return num;
     return fibonachi(num - 1) + fibonachi(num - 2);
 }
+int akkerman(int m, int n) {
+    if (m == 0) return n + 1;
+    else if (n == 0) return akkerman(m - 1, 1);
+    else return akkerman(m - 1, akkerman(m, n - 1));
+}
 int main() 
 {
     cout << "Введите число:" << endl;
     int fibnum;
     cin >> fibnum;
     cout << "Число Фибоначчи: " << fibonachi(fibnum) << endl;
+    cout << "Введите число m:" << endl;
+    int m;
+    cin >> m;
+    cout << "Введите число n:" << endl;
+    int n;
+    cin >> n;
+    cout << "Число Аккермана: " << akkerman(m, n) << endl;
+
 }
